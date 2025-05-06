d59f1baaa485c13402ab4861116cfc3d1c663130
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-05-06 21:05:20 +0300

    refactor stores and pits to be instances of player object

--- Ayoayo.java
+++ Ayoayo.java
@@ -1,15 +1,16 @@
+
+import java.util.Arrays;
+
 public class Ayoayo {
 
     private final Player[] players;
-    private final int[] store;
 
     public Ayoayo(){
         this.players = new Player[2];
-        this.store = new int[2];
     }
 
     public Player createPlayer(String name){
-        return new Player(name);
+        return new Player(name, 0, new int[]{4, 4, 4, 4, 4, 4});
     }
 
     public void setPlayer1(Player player){
@@ -20,17 +21,25 @@ public class Ayoayo {
         this.players[1] = player;
     }
 
+    public void playGame(int player, int pit){
+        
+    }
+
     public void printBoard(){
         int index = 0;
         for (Player player : players) {
             System.out.println("player"+ (index + 1) +": " + player.getName() + "\n");
-            System.out.println("store: " + store[index] + " seeds\n");
+            
+            System.out.println("store: " + player.getStore() + " seeds\n");
+            
+            System.out.println(Arrays.toString(player.getPit()) + "\n");
+            
             index++;
         }
     }
 
     public void returnWinner(){
-
+        
     }
 
 
--- Player.java
+++ Player.java
@@ -1,11 +1,49 @@
 public class Player {
     private String name;
+    private int store;
+    private int[] pit;
 
-    public Player(String name){
+    public Player(String name, int store, int[] pit){
         this.name = name;
+        this.store = store;
+        this.pit = pit;
     }
 
     public String getName(){
         return name;
     }
+
+    public int getStore(){
+        return store;
+    }
+
+    public int[] getPit(){
+        return pit;
+    }
+
+    public void addToStore(int seeds){
+        store += seeds;
+    }
+
+    public void addToPit(int index, int seeds){
+        pit[index] += seeds;
+    }
+
+    public void removeFromStore(int seeds){
+        if(store >= seeds){
+            store -= seeds;
+        } else{
+            System.out.println("Not enough seeds in store to remove.");
+        }
+    }
+
+    public void removeFromPit(int index, int seeds){
+        if(pit[index] >= seeds){
+            pit[index] -= seeds;
+        } else{
+            System.out.println("Not enough seeds in pit to remove.");
+        }
+    }
 }
+
+
