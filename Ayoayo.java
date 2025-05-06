ead75dd69a8ba4e0c8b9dc5e0384968e736a8506
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-05-06 15:49:54 +0300

    player class init

--- Ayoayo.java
+++ Ayoayo.java
@@ -1,15 +1,46 @@
 public class Ayoayo {
 
+    private final Player[] players;
+    private final int[] store;
+
     public Ayoayo(){
+        this.players = new Player[2];
+        this.store = new int[2];
+    }
+
+    public Player createPlayer(String name){
+        return new Player(name);
+    }
+
+    public void setPlayer1(Player player){
+        this.players[0] = player;
+    }
+
+    public void setPlayer2(Player player){
+        this.players[1] = player;
+    }
+
+    public void printBoard(){
+        int index = 0;
+        for (Player player : players) {
+            System.out.println("player"+ (index + 1) +": " + player.getName() + "\n");
+            System.out.println("store: " + store[index] + " seeds\n");
+            index++;
+        }
+    }
+
+    public void returnWinner(){
 
     }
 
-    public void hello(){
-        System.out.println("Hello World");
-    }
 
     public static void main(String[] args) {
-        Ayoayo run = new Ayoayo();
-        run.hello();
+        Ayoayo game = new Ayoayo();
+        Player p1 = game.createPlayer("Player 1");
+        game.setPlayer1(p1);
+        Player p2 = game.createPlayer("Player 2");
+        game.setPlayer2(p2);
+        
+        game.printBoard();
     }
 }
