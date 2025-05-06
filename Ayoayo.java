01b089bfe71cb39cfebeb27e924d8c4dbed9185b
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-05-06 09:45:11 +0300

    init class

---
+++ Ayoayo.java
@@ -0,0 +1,15 @@
+public class Ayoayo {
+
+    public Ayoayo(){
+
+    }
+
+    public void hello(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        Ayoayo run = new Ayoayo();
+        run.hello();
+    }
+}
